0eadca2cdfcb8c96989cd06a3b8778782a5bcb31
EVENTIM\jjahr <jannis.jahr@hotmail.de>
2017-02-15 16:09:28 +0100

    adjusted gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sftp = require('gulp-sftp');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	sftp = require('gulp-sftp'),
+	watch = require('gulp-watch');
+
+var source = './src',
+	dist = './dist';
+
 gulp.task('styles', function() {
     gulp.src('./src/stylesheets/**/*.scss')
         .pipe(sass().on('error', sass.logError))
@@ -12,11 +17,18 @@ gulp.task('default',function() {
     gulp.watch('stylesheets/**/*.scss',['styles']);
 });
 
+//copy task
+gulp.task('watch-folder', function() {
+	gulp.src(source + '/**/*', {base: source})
+	   .pipe(watch(source, {base: source}))
+	   .pipe(gulp.dest(dist));
+})
+
 gulp.task('deploy', function() {
 	return gulp.src('./src/**')
 	        .pipe(sftp({
 	            host: 'home669624775.1and1-data.host',
 	            remotePath: './htdocs',
 				auth: 'keyShort'
-	        }));
+	}));
 })
